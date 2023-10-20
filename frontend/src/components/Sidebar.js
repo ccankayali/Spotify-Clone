c9eb483a1adf42dfddad7c7b44b1db7eebf386ca
Can Kayalı <volkand135@gmail.com>
2023-10-20 23:34:34 +0300

    Spotify-Clone

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from '../img/logo.svg'
+import "../style.css";
 
 function Sidebar() {
   return (
-    <aside className="w-60 pt-6">
-      <img src={logo} alt="logo" className="w-40 mx-auto" />
+    <aside className="w-50 py-2 pt-1">
+      <img src={logo} alt="logo" className="w-30" />
     </aside>
     )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "./style.css";
+import "../src/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
