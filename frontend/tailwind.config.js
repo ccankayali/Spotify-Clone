85cfa9e8662057677eac224f41fe417460d31a17
Can Kayalı <volkand135@gmail.com>
2023-10-20 16:57:07 +0300

    Spotify-Clone

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    extend: {
+      colors: {
+        backdrop: '#121212',
+        primary: '#1DB954',
+        active: '#282828',
+        link: '#b3b3b3',
+        footer: '#181818'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
