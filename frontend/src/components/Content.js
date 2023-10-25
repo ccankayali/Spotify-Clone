a417cd8df40c590c1b328720cbc3adc08f90a030
Can Kayalı <volkand135@gmail.com>
2023-10-25 19:37:13 +0300

    Spotify-Clone

--- frontend/src/components/Content.js
+++ frontend/src/components/Content.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import Navbar from './Navbar';
-import { Routes, Route } from "react-router-dom";
-import Home from '../views/Home';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar'
+
+import Home from '../views/Home'
+import Search from '../views/Search'
+import Collection from '../views/Collection'
 
 function Content() {
-  return (
-    <main className="flex-auto">
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-    </main>
-  )
+	return (
+		<main className="flex-auto overflow-auto">
+			<Navbar />
+			<div className="px-8 py-5">
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/collection" element={<Collection />} />
+				</Routes>
+			</div>
+		</main>
+	)
 }
 
 export default Content
---
+++ frontend/src/components/Sidebar/DownloadApp.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// function DownloadApp() {
+//   return (
+//     <div>DownloadApp</div>
+//   )
+// }
+
+// export default DownloadApp
