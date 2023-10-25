2d55c8c056ed3a8fe3f1b4cb7af4d5028f16b871
Can Kayalı <volkand135@gmail.com>
2023-10-25 18:03:50 +0300

    Spotify-Clone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import BottomBar from './components/BottomBar';
 import Content from './components/Content';
 import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <div className="wrapper">
         <Sidebar />
         <Content />
@@ -12,7 +13,7 @@ function App() {
       <div>
         <BottomBar />
       </div>
-    </>  
+    </Router>  
   );
 }
 
